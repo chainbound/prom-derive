e35ad14005a54190284b180016d6662b3dddf85c
Jonas Bostoen <jonasbostoen@fastmail.com>
2025-10-30 09:29:34 +0100

    docs: add `metrics` macro docs

--- prometric-derive/src/lib.rs
+++ prometric-derive/src/lib.rs
@@ -1,3 +1,5 @@
+//! This crate contains the attribute macro for generating Prometheus metrics.
+//! Refer to the [metrics] attribute documentation for more information.
 use proc_macro::TokenStream;
 use syn::{ItemStruct, parse_macro_input};
 
@@ -6,7 +8,75 @@ use crate::expand::MetricsAttr;
 mod expand;
 mod utils;
 
-/// This macro generates a struct with embedded Prometheus metrics.
+/// This attribute macro instruments all of the struct fields with Prometheus metrics according to the attributes on the fields.
+/// It also generates an ergonomic accessor API for each of the defined metrics.
+///
+/// # Example
+/// ```rust
+/// use prometric_derive::metrics;
+/// use prometric::{Counter, Gauge, Histogram};
+///
+/// // The `scope` attribute is used to set the prefix for the metric names in this struct.
+/// #[metrics(scope = "app")]
+/// struct AppMetrics {
+///     /// The total number of HTTP requests.
+///     #[metric(rename = "http_requests_total", labels = ["method", "path"])]
+///     http_requests: Counter,
+///
+///     /// The duration of HTTP requests.
+///     #[metric(labels = ["method", "path"])]
+///     http_requests_duration: Histogram,
+///
+///     /// This doc comment will be overwritten by the `help` attribute.
+///     #[metric(rename = "current_active_users", labels = ["service"], help = "The current number of active users.")]
+///     current_users: Gauge,
+///
+///     /// The total number of errors.
+///     #[metric]
+///     errors: Counter,
+/// }
+///
+/// // Build the metrics struct with static labels, which will initialize and register the metrics with the default registry.
+/// // A custom registry can be used by passing it to the builder using `with_registry`.
+/// let metrics = AppMetrics::builder().with_label("host", "localhost").with_label("port", "8080").build();
+///
+/// // Metric fields each get an accessor method generated, which can be used to interact with the metric.
+/// // The arguments to the accessor method are the labels for the metric.
+/// metrics.http_requests("GET", "/").inc();
+/// metrics.http_requests_duration("GET", "/").observe(1.0);
+/// metrics.current_users("service-1").set(10);
+/// metrics.errors().inc();
+/// ```
+///
+/// # Sample Output
+/// ```text
+/// # HELP app_current_active_users The current number of active users.
+/// # TYPE app_current_active_users gauge
+/// app_current_active_users{host="localhost",port="8080",service="service-1"} 10
+/// # HELP app_errors_total The total number of errors.
+/// # TYPE app_errors_total counter
+/// app_errors_total{host="localhost",port="8080"} 1
+/// # HELP app_http_requests_duration The duration of HTTP requests.
+/// # TYPE app_http_requests_duration histogram
+/// app_http_requests_duration_bucket{host="localhost",method="GET",path="/",port="8080",le="0.005"} 0
+/// app_http_requests_duration_bucket{host="localhost",method="GET",path="/",port="8080",le="0.01"} 0
+/// app_http_requests_duration_bucket{host="localhost",method="GET",path="/",port="8080",le="0.025"} 0
+/// app_http_requests_duration_bucket{host="localhost",method="GET",path="/",port="8080",le="0.05"} 0
+/// app_http_requests_duration_bucket{host="localhost",method="GET",path="/",port="8080",le="0.1"} 0
+/// app_http_requests_duration_bucket{host="localhost",method="GET",path="/",port="8080",le="0.25"} 0
+/// app_http_requests_duration_bucket{host="localhost",method="GET",path="/",port="8080",le="0.5"} 0
+/// app_http_requests_duration_bucket{host="localhost",method="GET",path="/",port="8080",le="1"} 1
+/// app_http_requests_duration_bucket{host="localhost",method="GET",path="/",port="8080",le="2.5"} 1
+/// app_http_requests_duration_bucket{host="localhost",method="GET",path="/",port="8080",le="5"} 1
+/// app_http_requests_duration_bucket{host="localhost",method="GET",path="/",port="8080",le="10"} 1
+/// app_http_requests_duration_bucket{host="localhost",method="GET",path="/",port="8080",le="+Inf"} 1
+/// app_http_requests_duration_sum{host="localhost",method="GET",path="/",port="8080"} 1
+/// app_http_requests_duration_count{host="localhost",method="GET",path="/",port="8080"} 1
+/// # HELP app_http_requests_total The total number of HTTP requests.
+/// # TYPE app_http_requests_total counter
+/// app_http_requests_total{host="localhost",method="GET",path="/",port="8080"} 1
+/// ```
+///
 #[proc_macro_attribute]
 pub fn metrics(attr: TokenStream, item: TokenStream) -> TokenStream {
     // NOTE: We use `proc_macro_attribute` here because we're actually rewriting the struct. Derive macros are additive.

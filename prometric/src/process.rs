5a13d2e6423ccd9a7a1f52e0214d510103b5ebcd
Jonas Bostoen <jonasbostoen@fastmail.com>
2025-11-13 18:31:20 +0100

    feat(process): add thread busyness stats

--- prometric/src/process.rs
+++ prometric/src/process.rs
@@ -1,5 +1,5 @@
 use prometheus::{
-    Gauge, Registry,
+    Gauge, GaugeVec, Opts, Registry,
     core::{AtomicU64, GenericGauge},
 };
 use sysinfo::{CpuRefreshKind, MemoryRefreshKind, Pid, ProcessRefreshKind, RefreshKind, System};
@@ -88,10 +88,36 @@ impl ProcessCollector {
             self.sys.used_memory() as f64 / self.sys.total_memory() as f64 * 100.0;
 
         let process = self.sys.process(self.pid).unwrap();
+        let cpu_usage = process.cpu_usage() / self.cores as f32;
+
+        // Collect thread stats
+        process.tasks().map(|tasks| {
+            tasks.iter().for_each(|pid| {
+                let Some(thread) = self.sys.process(*pid) else {
+                    return;
+                };
+
+                let pid = pid.to_string();
+                let name = thread.name().to_str().unwrap_or_else(|| pid.as_str());
+
+                // Calculate the busy ratio of the thread as a percentage of the CPU usage of
+                // the process.
+                let busy_ratio = if process.cpu_usage() > 0.0 {
+                    thread.cpu_usage() / process.cpu_usage()
+                } else {
+                    0.0
+                };
+
+                self.metrics
+                    .thread_stats
+                    .with_label_values(&[pid.as_str(), name])
+                    .set(busy_ratio as f64);
+            });
+        });
+
         let threads = process.tasks().map(|tasks| tasks.len()).unwrap_or(0);
         let open_fds = process.open_files().unwrap_or(0);
         let max_fds = process.open_files_limit().unwrap_or(0);
-        let cpu_usage = process.cpu_usage() / self.cores as f32;
         let resident_memory = process.memory();
         let resident_memory_usage = resident_memory as f64 / self.sys.total_memory() as f64;
         let disk_usage = process.disk_usage().total_written_bytes;
@@ -147,6 +173,8 @@ pub struct ProcessMetrics {
     max_fds: UintGauge,
     /// The total written bytes to disk by the process.
     disk_written_bytes: UintCounter,
+    /// The statistics of the threads used by the process.
+    thread_stats: GaugeVec,
 
     /// The duration of the associated collection routine in seconds.
     collection_duration: Gauge,
@@ -212,6 +240,11 @@ impl ProcessMetrics {
             "The total written bytes to disk by the process.",
         )
         .unwrap();
+        let thread_stats: GaugeVec = GaugeVec::new(
+            Opts::new("process_thread_stats", "The statistics of the threads used by the process."),
+            &["pid", "name"],
+        )
+        .unwrap();
 
         let collection_duration = Gauge::new(
             "process_collection_duration_seconds",
@@ -234,6 +267,8 @@ impl ProcessMetrics {
         registry.register(Box::new(open_fds.clone())).unwrap();
         registry.register(Box::new(max_fds.clone())).unwrap();
         registry.register(Box::new(disk_written_bytes.clone())).unwrap();
+        registry.register(Box::new(thread_stats.clone())).unwrap();
+
         registry.register(Box::new(collection_duration.clone())).unwrap();
 
         Self {
@@ -250,6 +285,7 @@ impl ProcessMetrics {
             open_fds,
             max_fds,
             disk_written_bytes,
+            thread_stats,
             collection_duration,
         }
     }
